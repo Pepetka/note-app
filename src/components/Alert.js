7c15126c9a5ad28652f28138e396510925c7fc6e
Vlad Kuznetsov <vlad.kznetsov@gmail.com>
2022-08-21 12:11:08 +0300

    add animations

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,18 +1,28 @@
 import React from "react"
+import { CSSTransition } from "react-transition-group"
 import { AlertContext } from "../context/alert/alertContext"
 
 const Alert = () => {
 	const { alert, hideAlert } = React.useContext(AlertContext)
 
-	if (!alert.visible) return null
-
 	return (
-		<div className={`alert alert-${alert.type} alert-dismissible`}>
-			<strong>Attention!</strong>
-			<br />
-			{alert.text}
-			<button onClick={hideAlert} type='button' className='btn-close'></button>
-		</div>
+		<CSSTransition
+			mountOnEnter
+			unmountOnExit
+			in={alert.visible}
+			timeout={{
+				enter: 500,
+				exit: 200,
+			}}
+			classNames='animate-alert'
+		>
+			<div className={`alert alert-${alert.type} alert-dismissible`}>
+				<strong>Attention!</strong>
+				<br />
+				{alert.text}
+				<button onClick={hideAlert} type='button' className='btn-close'></button>
+			</div>
+		</CSSTransition>
 	)
 }
 
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,28 +1,28 @@
 import React from "react"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 import { FirebaseContext } from "../context/firebase/firebaseContext"
 
 const Notes = () => {
 	const { notes, removeNote } = React.useContext(FirebaseContext)
 
 	return (
-		<ul className='list-group'>
+		<TransitionGroup component='ul' className='list-group'>
 			{notes.map((note) => (
-				<li
-					key={note.id}
-					className='note list-group-item d-flex justify-content-between align-items-center'
-				>
-					<div className='d-flex justify-content-between align-items-center'>
-						<strong>{note.title}</strong>
-						<small>{note.date}</small>
-					</div>
-					<button
-						onClick={() => removeNote(note.id)}
-						type='button'
-						className='btn btn-outline-danger btn-close'
-					></button>
-				</li>
+				<CSSTransition key={note.id} timeout={500} classNames='animate-note'>
+					<li className='note list-group-item d-flex justify-content-between align-items-center'>
+						<div className='d-flex justify-content-between align-items-center'>
+							<strong>{note.title}</strong>
+							<small>{note.date}</small>
+						</div>
+						<button
+							onClick={() => removeNote(note.id)}
+							type='button'
+							className='btn btn-outline-danger btn-close'
+						></button>
+					</li>
+				</CSSTransition>
 			))}
-		</ul>
+		</TransitionGroup>
 	)
 }
 
