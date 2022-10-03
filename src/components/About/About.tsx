17cac2bb818d4229a6e827b6ab8753f6833068ff
Vlad Kuznetsov <vlad.kznetsov@gmail.com>
2022-10-03 12:50:16 +0300

    new version 4.2.0

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,7 +9,7 @@ export const About = () => {
 		<div className={cls.About}>
 			<h2 className={cls.title}>{t('About Note App')}</h2>
 			<p className={cls.text}>
-				<strong className={cls.strong}>{t('App version')}</strong> {'v4.1.0'}
+				<strong className={cls.strong}>{t('App version')}</strong> {'v4.2.0'}
 				<br />
 				<strong className={cls.strong}>{t('Description')}</strong> {' '}
 				{t('App description')}
